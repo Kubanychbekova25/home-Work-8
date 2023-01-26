cecb4d8ed2a5d4e6531312c306a7c76fa6e7382d
Kubanychbekova25 <122251956+Kubanychbekova25@users.noreply.github.com>
2023-01-26 16:54:16 +0600

    Updates

---
+++ precache-manifest.206f28d3af7c346a9a7efbc247ec83c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce8675f169010a0dc9c5",
+    "url": "/home-Work-8/static/css/main.ca75bfd4.chunk.css"
+  },
+  {
+    "revision": "ce8675f169010a0dc9c5",
+    "url": "/home-Work-8/static/js/main.ce8675f1.chunk.js"
+  },
+  {
+    "revision": "d0acc314c0a6c805f44b",
+    "url": "/home-Work-8/static/js/1.d0acc314.chunk.js"
+  },
+  {
+    "revision": "fa90031918ba56719c3a",
+    "url": "/home-Work-8/static/js/2.fa900319.chunk.js"
+  },
+  {
+    "revision": "4f89f82709b7b4d3196b",
+    "url": "/home-Work-8/static/js/runtime~main.4f89f827.js"
+  },
+  {
+    "revision": "cacbbe2c11eff0972ad51292d37d2724",
+    "url": "/home-Work-8/index.html"
+  }
+];
